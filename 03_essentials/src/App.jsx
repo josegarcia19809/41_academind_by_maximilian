a5ad46e86476aa9e11794866d0525ab17a8bd210
Jose Garcia <josegarcia19809@gmail.com>
2023-12-16 14:42:17 -0600

    Essentials React

---
+++ 03_essentials/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import MainGoalApp from "./components/MainGoalApp.jsx";
+
+// DEFINE YOUR COMPONENT HERE
+// IMPORTANT: Add "export" in front of your component - otherwise the automated tests won't work
+// ...
+
+// DO NOT EDIT THE APP COMPONENT NAME OR CONTENT
+// Except for outputting your custom component
+function App() {
+
+    return (
+        <>
+            <MainGoalApp/>
+        </>
+    )
+}
+
+export default App
---
+++ 03_essentials/src/components/MainGoal.jsx
@@ -0,0 +1,10 @@
+
+const MainGoal = () => {
+    return (
+        <>
+            <p>My main goal: Learn React in-depth and from the ground up</p>
+        </>
+    );
+};
+
+export default MainGoal;
