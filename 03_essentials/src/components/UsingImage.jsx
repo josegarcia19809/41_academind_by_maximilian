d81e2a6847a04c3f5713a462e1b563ea77991033
Jose Garcia <josegarcia19809@gmail.com>
2023-12-17 15:41:21 -0600

    Using images

---
+++ 03_essentials/src/components/UsingImage.jsx
@@ -0,0 +1,11 @@
+import componentsImg from '../assets/components.png';
+
+const UsingImage = () => {
+    return (
+        <div>
+            <img src={componentsImg} alt="Stylized atom" />
+        </div>
+    );
+};
+
+export default UsingImage;
