14036760e7dd60363f23a386b2d130d353e00c63
Jose Garcia <josegarcia19809@gmail.com>
2023-12-17 16:17:19 -0600

    Making Components Reusable with components

--- 03_essentials/src/App.jsx
+++ 03_essentials/src/App.jsx
@@ -3,13 +3,14 @@ import MainGoalApp from "./components/MainGoalApp.jsx";
 import ValoresDinamicos from "./components/ValoresDinamicos.jsx";
 import UsingImage from "./components/UsingImage.jsx";
 import User from "./components/User.jsx";
-
+import {UsingPropsApp} from "./components/UsingProps.jsx";
+import componentsImg from "./assets/components.png";
 
 function App() {
 
     return (
         <>
-            <User/>
+            <UsingPropsApp/>
         </>
     )
 }
--- 03_essentials/src/components/UsingProps.jsx
+++ 03_essentials/src/components/UsingProps.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import componentsImg from "../assets/components.png";
+import configImg from "../assets/config.png";
 
-const UsingProps = () => {
+const UsingProps = (props) => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <li>
+                <img src={props.image} alt={props.title}/>
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+            </li>
+        </>
     );
 };
+
+export const UsingPropsApp = () => {
+    return (
+        <>
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <UsingProps
+                            title="Components"
+                            description="The core UI building block - compose the user interface
+                            by combining multiple components."
+                            image={componentsImg}
+                        />
+                        <UsingProps
+                            title="Config"
+                            description="Return (potentially dynamic) HTML(ish) code to
+                            define the actual markup that will be rendered."
+                            image={configImg}
+                        />
+                    </ul>
+                </section>
+            </main>
+        </>
+    );
+}
 
 export default UsingProps;
