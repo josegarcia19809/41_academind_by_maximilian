707135f017aee078636592822b0e176df86eb01c
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-13 20:45:17 -0600

    React essentials: Storing Components in Files

--- 03_essentials/src/App.jsx
+++ 03_essentials/src/App.jsx
@@ -1,19 +1,11 @@
-import './App.css'
-import MainGoalApp from "./components/MainGoalApp.jsx";
-import ValoresDinamicos from "./components/ValoresDinamicos.jsx";
-import UsingImage from "./components/UsingImage.jsx";
-import User from "./components/User.jsx";
-import {UsingPropsApp} from "./components/UsingProps.jsx";
-import componentsImg from "./assets/components.png";
-import Header from "./components/Header.jsx";
+import App07 from "./components/07-storing-cmp-in-files/App_07.jsx";
+
 
 function App() {
 
     return (
         <>
-            <div>
-                <Header />
-            </div>
+            <App07/>
         </>
     )
 }
---
+++ 03_essentials/src/components/07-storing-cmp-in-files/App_07.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from "./Header.jsx";
+import {CORE_CONCEPTS} from "./data.js";
+import CoreConcept from "./CoreConcept.jsx";
+
+function App07() {
+    return (
+        <>
+            <Header/>
+            <main>
+                <section id={"core-concepts"}>
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
+
+                    </ul>
+                </section>
+            </main>
+        </>
+    );
+}
+
+export default App07;
