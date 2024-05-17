dc26dca6a928bd0456b22ae450fe03cbf4fd79e8
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-16 20:02:03 -0600

    React essentials - Exercise: Configuring Event Handlers

--- 03_essentials/src/App.jsx
+++ 03_essentials/src/App.jsx
@@ -1,11 +1,14 @@
 
-import App_07 from "./components/07-storing-cmp-in-files/App_07.jsx";
+//import App_07 from "./components/07-storing-cmp-in-files/App_07.jsx";
+
+import App08 from "./components/06_configuring_event_handlers/App_08.jsx";
 
 function App() {
 
     return (
         <>
-            <App_07/>
+            {/*//<App_07/>*/}
+            <App08/>
         </>
     )
 }
