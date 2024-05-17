78ad00b06af512780af04de4f80ffa9a1293b218
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-17 07:15:31 -0600

    React essentials - Exercise: Working with State

--- 03_essentials/src/main.jsx
+++ 03_essentials/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import "./index.css"
+//import App from './App.jsx'
+//import "./index.css"
+
+import App_09 from "./components/09_working_with_state/App_09.jsx";
+import "./components/09_working_with_state/index.css";
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <App_09 />
 )
