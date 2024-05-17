9c433c9e704dc8b96d6e00fe48a9bdf4e0d1777b
josegarcia19809 <josegarcia19809@gmail.com>
2024-05-17 09:25:24 -0600

    React essentials - Exercise: Conditional Content

--- 03_essentials/src/main.jsx
+++ 03_essentials/src/main.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import "./index.css"
+// import App from './App.jsx'
+// import "./index.css"
+
+import App_10 from "./components/10_conditional_content/App_10.jsx";
+import './components/10_conditional_content/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <App_10 />
 )
