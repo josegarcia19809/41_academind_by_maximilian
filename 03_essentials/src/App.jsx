9d23035bab101ab8ccde70d9eadc5c53ee395af0
josegarcia19809 <josegarcia19809@gmail.com>
2024-06-22 19:35:10 -0600

    React essentials - Deep Dive - Exercise: Forwarding Props

--- 03_essentials/src/App.jsx
+++ 03_essentials/src/App.jsx
@@ -1,12 +1,15 @@
-import App_Proyecto from "./components/proyecto_principal/App_Proyecto.jsx";
+// import App_Proyecto from "./components/proyecto_principal/App_Proyecto.jsx";
 
 
+
+import App_14 from "./components/14_forwarding_props/App_14.jsx";
 
 function App() {
 
     return (
         <>
-            <App_Proyecto/>
+            {/*<App_Proyecto/>*/}
+            <App_14/>
 
         </>
     )
