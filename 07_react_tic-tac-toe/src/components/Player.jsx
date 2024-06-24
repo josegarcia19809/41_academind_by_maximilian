4eeb3355406f0665395d16cad5375cfc3c9cc3fa
josegarcia19809 <josegarcia19809@gmail.com>
2024-06-23 20:10:16 -0600

    React essentials - tic-tac-toe: Working with State

--- 07_react_tic-tac-toe/src/components/Player.jsx
+++ 07_react_tic-tac-toe/src/components/Player.jsx
@@ -1,12 +1,28 @@
+import {useState} from "react";
+
 function Player({name, symbol}) {
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+    }
+
     return (
         <>
             <li>
                 <span className="player">
-                    <span className="player-name">{name}</span>
+                    {
+                        !isEditing ? (<span className="player-name">{name}</span>) :
+                            (<input type="text" required/>)
+                    }
+
                     <span className="player-symbol">{symbol}</span>
                 </span>
-                <button>Edit</button>
+                <button
+                    onClick={() => handleEditClick()}
+                >Edit
+                </button>
             </li>
         </>
     );
