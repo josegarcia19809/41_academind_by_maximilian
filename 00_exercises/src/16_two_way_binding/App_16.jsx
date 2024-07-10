96dd29c0a2b1ef07620b0833d6ca42c7848b3375
josegarcia19809 <josegarcia19809@gmail.com>
2024-07-10 11:10:59 -0600

    React essentials - Exercise: 16 Two-Way-Binding

--- 00_exercises/src/16_two_way_binding/App_16.jsx
+++ 00_exercises/src/16_two_way_binding/App_16.jsx
@@ -1,24 +1,40 @@
 import Review from "./Review.jsx";
 import "./index.css";
+import React from 'react';
+
 // don't change the Component name "App"
 function App_16() {
+    const [feedback, setFeedback] = React.useState("");
+    const [studentName, setStudentName] = React.useState("");
+
+    function handleChangeStudentName(event) {
+        setStudentName(event.target.value);
+    }
+
+    function handleChangeFeedback(event) {
+        setFeedback(event.target.value);
+    }
+    
     return (
         <>
             <section id="feedback">
                 <h2>Please share some feedback</h2>
                 <p>
                     <label>Your Feedback</label>
-                    <textarea />
+                    <textarea value={feedback} onChange={handleChangeFeedback}/>
                 </p>
                 <p>
                     <label>Your Name</label>
-                    <input type="text" />
+                    <input type="text" value={studentName} onChange={handleChangeStudentName}/>
                 </p>
             </section>
             <section id="draft">
                 <h2>Your feedback</h2>
 
-                <Review />
+                <Review
+                    feedback={feedback}
+                    student={studentName}
+                />
 
                 <p>
                     <button>Save</button>
--- 00_exercises/src/16_two_way_binding/Review.jsx
+++ 00_exercises/src/16_two_way_binding/Review.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line react/prop-types
 export default function Review({ feedback, student }) {
     return (
         <figure>
