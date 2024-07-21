8b800b60ea257bb6ae84b9b598223c7294935072
josegarcia19809 <josegarcia19809@gmail.com>
2024-07-20 20:35:20 -0600

    Ejercicios con JavaScript: incremento decremento

---
+++ 02_exercises_js/src/App.jsx
@@ -0,0 +1,12 @@
+import IncrementoDecremento
+    from "./components/reducer/IncrementoDecremento/IncrementoDecremento.jsx";
+
+function App() {
+    return (
+        <>
+            <IncrementoDecremento/>
+        </>
+    );
+}
+
+export default App;
